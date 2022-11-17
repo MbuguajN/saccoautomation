a64398bc81b53203a02ee2572e129a1e11436094
bradleygichuru <prz03@protonmail.com>
2022-11-17 22:31:35 +0000

    feat: adds guarantor selection

--- src/requestloan.jsx
+++ src/requestloan.jsx
@@ -2,9 +2,21 @@ import Layout from "./layout";
 import { useState } from "react";
 import { useEffect } from "react";
 
+import { useForm } from "react-hook-form";
 export default function RequestLoan() {
   const [allowRequest, setAlLowRequest] = useState(false);
   const [guarantors, setGuarantors] = useState([]);
+  const [amount, setLoanAmount] = useState();
+  const [selectedGuarantor, setSelectedGuarantor] = useState("");
+
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm();
+  const onsubmit = (data) => console.log(data);
+
   const getGuarantors = () => {
     fetch("http://localhost:3002/guarantors", {
       method: "GET",
@@ -17,7 +29,7 @@ export default function RequestLoan() {
         setGuarantors(data);
       });
   };
-  const stkPushSim = () => {
+  /* const stkPushSim = () => {
     fetch("http://localhost:3002/stk", {
       method: "POST",
       headers: {
@@ -28,9 +40,9 @@ export default function RequestLoan() {
         amount: 1,
       }),
     });
-  };
+  };*/
   useEffect(() => {
-    stkPushSim();
+    //stkPushSim();
     getGuarantors();
   }, []);
   if (guarantors) {
@@ -39,11 +51,7 @@ export default function RequestLoan() {
   const rows = guarantors?.results?.map((guarantor, index) => {
     return (
       <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
+        <th></th>
         <td>
           <div className="flex items-center space-x-3">
             <div className="avatar"></div>
@@ -69,11 +77,7 @@ export default function RequestLoan() {
         <table className="table w-full ">
           <thead>
             <tr>
-              <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
-              </th>
+              <th></th>
               <th>Guarantor</th>
               <th>Id Number</th>
             </tr>
@@ -82,34 +86,52 @@ export default function RequestLoan() {
           <tfoot></tfoot>
         </table>
       </div>
-      <div className="stats bg-accent text-primary-content my-3">
-        <div className="stat">
-          <div className="stat-title">loan limit</div>
-          <div className="stat-value">KSH 89,400</div>
-          <label className="label">
-            <span className="label-text">Enter amount</span>
-          </label>
-          <label className="input-group">
-            <span>Amount</span>
-            <input
-              type="text"
-              placeholder="enter amount"
-              className="input input-bordered text-black"
-            />
-            <span>ksh</span>
-          </label>
-          <div className="stat-actions ">
-            <button className="btn btn-sm btn-success bg-rose-400">
-              Request loan
-            </button>
+      <form onSubmit={handleSubmit(onsubmit)}>
+        <select {...register("guarantor",{required:"select a guarantor"})} className="select select-bordered m-4 w-full max-w-xs">
+          <option disabled selected>
+            Pick your Loan guarantor
+          </option>
+          {guarantors?.results?.map((guarantor, index) => {
+            return (
+              <option
+                key={index}
+                val={guarantor.username}
+              >
+                {guarantor.username}
+              </option>
+            );
+          })}
+        </select>
+        <div className="stats bg-accent text-primary-content my-3">
+          <div className="stat">
+            <div className="stat-title">loan limit</div>
+            <div className="stat-value">KSH 89,400</div>
+            <label className="label">
+              <span className="label-text">Enter amount</span>
+            </label>
+            <label className="input-group">
+              <span>Amount</span>
+              <input
+                type="number"
+                {...register("loanAmount",{required:"set a loan amount before submitting"})}
+                placeholder="enter amount"
+                className="input input-bordered text-black"
+              />
+              <span>ksh</span>
+            </label>
+            <div className="stat-actions ">
+              <button className="btn btn-sm btn-success bg-rose-400">
+                Request loan
+              </button>
+            </div>
+          </div>
+
+          <div className="stat">
+            <div className="stat-title">loan balance</div>
+            <div className="stat-value">KSH 0</div>
           </div>
         </div>
-
-        <div className="stat">
-          <div className="stat-title">loan balance</div>
-          <div className="stat-value">KSH 0</div>
-        </div>
-      </div>
+      </form>
     </Layout>
   );
 }
